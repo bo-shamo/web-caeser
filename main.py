337d1447282ef01a564284d51e14d43d191302ff
Dan Roesch <danielar2143@gmail.com>
2019-06-28 23:34:12 -0500

    added base encrypt page and completed form

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request
+    #TODO: Add Caesar rotate string
+
 
 app = Flask(__name__)
 app.config['DEBUG']= True
@@ -24,7 +26,14 @@ form = """
     </style>
   </head>
   <body>
-    
+    <form action="/encrypt" method="POST">
+      <label for="rot">Rotate by:
+        <input id="rot" name="rot" type="text" value="0" />
+      </label>
+      <textarea id="text" name="text">
+      </textarea>
+      <input type="submit" value="Submit Query" />
+    </form>
   </body>
 </html>
 """
@@ -32,5 +41,13 @@ form = """
 @app.route("/")
 def index():
   return form
+
+@app.route("/encrypt", methods=["POST"])
+def encrypt():
+    rotate = request.form['rot']
+    text = request.form['text']
+
+
+    #TODO:apply encrypting to the submitted text
 
 app.run()
